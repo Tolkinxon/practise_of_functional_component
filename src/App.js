186cd76e8e7662c94866660efdd6c06e25edb2d5
Tolkinxon <krosstony639@gmail.com>
2022-12-29 12:14:28 +0500

    copyied class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
-import './App.css';
+import './App.css'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      isCounting: false,
+    }
+  }
+
+  start = () => {
+    this.setState({ isCounting: true })
+
+    this.int = setInterval(() => {
+      this.setState({ count: this.state.count + 1 })
+    }, 1000)
+  }
+
+  stop = () => {
+    this.setState({ isCounting: false })
+    clearInterval(this.int)
+  }
+
+  restart = () => {
+    this.setState({ isCounting: false, count: 0 })
+    clearInterval(this.int)
    
-    </div>
-  )
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    const his = localStorage.getItem('second')
+    this.setState({count: +his})
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+    localStorage.setItem('second', this.state.count)
+  }
+
+  componentWillUnmount(){
+    console.log('componentWillUnmount')
+    clearInterval(this.int)
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <h2>{this.state.count}</h2>
+        {!this.state.isCounting ? (
+          <button onClick={this.start}>start</button>
+        ) : (
+          <button onClick={this.stop}>stop</button>
+        )}
+
+        <button onClick={this.restart}>restart</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
